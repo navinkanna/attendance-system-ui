7aa2ba5f3ed811be72c9b2828ad4ea99862c777d
Navin Kanna <navinkumark1997@gmail.com>
2026-07-27 16:38:19 -0500

    Register Class Initial Push and other changes

---
+++ src/pages/RecordAttendance.jsx
@@ -0,0 +1,10 @@
+function RecordAttendance() {
+  return (
+    <section className="page">
+      <h1>Record Attendance</h1>
+      <p>Mark attendance for a class here.</p>
+    </section>
+  )
+}
+
+export default RecordAttendance
